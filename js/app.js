2ec3e081ae218bc810be816103f7aa5e8fc5141f
Jeandre-jpg <190025@virtualwindow.co.za>
2020-06-02 21:07:20 +0200

    wrong

--- js/app.js
+++ js/app.js
@@ -1,50 +1,48 @@
-// const io = require('socket.io')(http);
+const express = require('express')
+const app = express()
 
 
-// let route = io.connect('http://localhost:3000/route');
+//set the template engine ejs
+app.set('view engine', 'ejs')
 
-// // route.on('welcome', (msg) => {
-// //     console.log('Received: ', msg);
-// // });
-
-// // route.emit('joinRoom', 'friendly')
+//middlewares
+app.use(express.static('public'))
 
 
-// // route.on('newUser', (res) => console.log(res));
+//routes
+app.get('/', (req, res) => {
+	res.render('index')
+})
 
-// // route.on('err', (err) => console.log(err));
-
-// // route.on('success', (res) => console.log(res));
-
-
-// const userName = 'Jeandre De Villiers';
-
-// let chat = io.connect('http://localhost:3000/chat');
-
-// function sendMessage(){
-//     let msg = document.getElementById('chat-input').value;
-//     console.log('Message: ', msg);
-
-//     input.value= ';'
-
-//     chat.emit('newMsg',  {userName, msg});
-// }
-
-// function addMessage(container, data){
-//     let elm = document.getElementById("p");
-//     elm.textContent = `${data.userName}: ${data.msg}`;
-//     container.appendChild(elm);
-// }
-
-// chat.on('newMessage', (data) => {
-//     console.log(`New message received from user: , ${data.userName}: ${data.msg}`);
-// });
-
-// const btn = document.getElementById('submissionBtn');
-
-// btn.onclick = () => {
-//     sendMessage();
-// }
+//Listen on port 3000
+server = app.listen(3000)
 
 
-// let container = document.getElementById('container');
+
+//socket.io instantiation
+const io = require("socket.io")(server)
+
+
+//listen on every connection
+io.on('connection', (socket) => {
+	console.log('New user connected')
+
+	//default username
+	socket.username = "Anonymous"
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
+
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
+})
