257c67ed53e193bc421ffae536144652fe0687c3
Jeandre-jpg <190025@virtualwindow.co.za>
2020-06-11 10:14:22 +0200

    made chat improvements

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('http').createServer(response);
 var fs = require('fs');
 var io = require('socket.io')(app);
+var fs =  require('fs'); 
 
 app.listen(3000);
 console.log("App running...");
@@ -26,6 +27,53 @@ function response(req, res) {
     );
 }
 
+var simpleRouter = function(request) {
+	var method = request.method;
+	var path = request.url;
+	var suppliedRoute = {method: method, path: path}
+	var routes = [
+	  {method: 'GET', path: '/', handler: handleFormGet},
+	  {method: 'POST', path: '/', handler: handleFormPost}
+	];
+	for (var i = 0; i < routes.length; i++) {
+	  var route = routes[i];
+	  if ( route.method === suppliedRoute.method &&
+		route.path === suppliedRoute.path ) {
+		return route.handler;
+	  }
+	}
+	return null;
+  }
+
+  var handleFormPost = function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/html"});
+	var payload = '';
+  
+	request.on('data', function (data) {
+	  payload += data;
+	});
+  
+	request.on('end', function () {
+	  var post = queryString.parse(payload);
+	  contacts.push(post['username']);
+	  response.writeHead(200, {"Content-Type": "text/html"});
+	  fs.readFile('templates/contacts.html', 'utf8', function(err, data) {
+		if (err) { throw err; }
+		var compiled = template(data, {username: post['username'], userList: contacts.join(",")});
+		response.write(compiled);
+		response.end();
+	  });
+	});
+  }
+  
+  var method = request.method;
+var path = request.url;
+var queryIndex = request.url.indexOf('?');
+if (queryIndex >= 0) {
+  path = request.url.slice(0, queryIndex)
+}
+var suppliedRoute = {method: method, path: path}
+
 io.on("connection", function(socket){
     socket.on("send message", function(sent_msg, callback){
 		sent_msg = "[ " + getCurrentDate() + " ]: " + sent_msg;
@@ -46,3 +94,4 @@ function getCurrentDate(){
 
 	return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
 }
+
